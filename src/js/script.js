34d84b382dc4a3ad815a978dbaf14674ecc5acd4
Aidar <robinzone-kruzo@mail.ru>
2019-02-19 00:46:18 +0600

    1-day.02

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
-window.addEventListener('DOMContentLoader',() => {
+window.addEventListener('DOMContentLoaded',()=>{
         const cartWrapper = document.querySelector('.cart__wrapper'),
-        cart = document.querySelector('.cart'),
-        close = document.querySelector('.cart__close'),
-        open = document.querySelector('#cart'),
-        goodsBtn = document.querySelectorAll('.goods__btn'),
-        products = document.querySelectorAll('.goods__items'),
-        confirm = document.querySelector('.confirm'),
-        badge = document.querySelector('.nav__badge'),
-        totalCost = document.querySelector('.cart__total>span'),
-        titles = document.querySelectorAll('.goods__title');
+                cart = document.querySelector('.cart'),
+                close = document.querySelector('.cart__close'),
+                open = document.querySelector('#cart'),
+                goodsBtn = document.querySelectorAll('.goods__btn'),
+                products = document.querySelectorAll('.goods__item'),
+                confirm = document.querySelector('.confirm'),
+                badge = document.querySelector('.nav__badge'),
+                totalCost = document.querySelector('.cart__total>span'),
+                titles = document.querySelectorAll('.goods__title');
 
     function openCart() {
         cart.style.display = 'block';  //почему тут не document.cart.style.display ?
@@ -24,10 +24,10 @@ window.addEventListener('DOMContentLoader',() => {
     close.addEventListener('click',closeCart);
 
     goodsBtn.forEach(function(btn, i){
-        btn.addEventListener('click', ()=>{
-            let item = products[i].cloneNode(true),
-                trigger = item.querySelector('button'),
-                removeBtn = document.createElement('div'),
+        btn.addEventListener('click', () => {
+            let item = products[i].cloneNode(true), 
+                trigger = item.querySelector('button'),   
+                removeBtn = document.createElement('div'),     
                 empty = cartWrapper.querySelector('.empty');
 
             trigger.remove();
